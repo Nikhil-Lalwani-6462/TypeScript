4e26d13173bf0d45772dcadaf4aa838a16d0f52e
Nikhil-Lalwani-6462 <nikhillalwani6462@gmail.com>
2026-08-01 14:37:01 -0700

    Assignment program implemented

---
+++ Basics/Variables and DataTypes/Assignment.ts
@@ -0,0 +1,23 @@
+// // Bonus Challenge
+
+// Create variables for yourself:
+
+// Name
+// Qualification
+// Experience
+// Company
+// Salary
+// Is Working
+let Employee = {
+
+    Name: "Nikhil Lalwani",
+    Qualification : "MCA",
+    Experience :"0.6 year",
+    Company : "Variance Infotech Pvt. Ltd.",
+    Salary :  16666,
+    IsWorking :  true
+};
+console.log(Employee);
+
+
+
