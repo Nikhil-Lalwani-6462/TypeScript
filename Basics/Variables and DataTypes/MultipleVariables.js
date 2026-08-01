cbd6623d7c4775c702043450d384ac41f9a328a9
Nikhil-Lalwani-6462 <nikhillalwani6462@gmail.com>
2026-08-01 14:14:42 -0700

    Initial comit - TypeScript basics

---
+++ Basics/Variables and DataTypes/MultipleVariables.js
@@ -0,0 +1,9 @@
+"use strict";
+let EmpName = "Nikhil Lalwani";
+let EmpAge = 26;
+let EmpSalary = 45000;
+let EmpDesignation = "Software Engineer";
+console.log(`Employee Name : ${EmpName}`);
+console.log(`Employee Age :${EmpAge}`);
+console.log(`Employee Designation : ${EmpDesignation}`);
+console.log(`Employee Saalry : ${EmpSalary}`);
---
+++ Basics/Variables and DataTypes/MultipleVariables.ts
@@ -0,0 +1,10 @@
+let EmpName: string = "Nikhil Lalwani";
+let EmpAge: number = 26;
+let EmpSalary: number = 45000
+let EmpDesignation : string = "Software Engineer"
+
+console. log(`Employee Name : ${EmpName}`);
+console.log(`Employee Age :${EmpAge}`);
+console.log(`Employee Designation : ${EmpDesignation}`);
+console.log(`Employee Saalry : ${EmpSalary}`);
+
